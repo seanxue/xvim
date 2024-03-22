9857545339086a8734036d4ed5e493f6d06bf021
Sean Xue <lianggui.xue@gmail.com>
2024-03-22 18:22:55 +0800

    remove deprecated gopls option

--- lua/rafi/plugins/extras/lang/go.lua
+++ lua/rafi/plugins/extras/lang/go.lua
@@ -45,7 +45,6 @@ return {
 							usePlaceholders = true,
 							completeUnimported = true,
 							staticcheck = true,
-							allowModfileModifications = true,
 							directoryFilters = {
 								"-.git",
 								"-.vscode",
