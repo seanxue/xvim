9164ca848a8a9272071c4c8b9dde5c66c0e9adaa
Sean Xue <lianggui.xue@gmail.com>
2024-11-11 11:15:31 +0800

    fix persisted.nvim config

--- lua/rafi/plugins/editor.lua
+++ lua/rafi/plugins/editor.lua
@@ -30,12 +30,11 @@ return {
 	-----------------------------------------------------------------------------
 	{
 		"olimorris/persisted.nvim",
-		event = "VimEnter",
-		priority = 1000,
+		lazy = false,
 		opts = {
-			save_dir = vim.fn.expand(vim.fn.stdpath("data") .. "/sessions/"),
 			autoload = true,
-			follow_cwd = false,
+			autosave = true,
+			use_git_branch = true,
 			ignored_dirs = { "/usr", "/opt", "~/.cache", vim.env.TMPDIR or "/tmp" },
 		},
 		config = function(_, opts)
@@ -45,68 +44,6 @@ return {
 				opts.autosave = false
 			end
 			require("persisted").setup(opts)
-		end,
-		init = function()
-			-- Detect if stdin has been provided.
-			vim.g.in_pager_mode = false
-			vim.api.nvim_create_autocmd("StdinReadPre", {
-				group = vim.api.nvim_create_augroup("rafi_persisted", {}),
-				callback = function()
-					vim.g.in_pager_mode = true
-				end,
-			})
-			-- Close all floats before loading a session. (e.g. Lazy.nvim)
-			vim.api.nvim_create_autocmd("User", {
-				group = "rafi_persisted",
-				pattern = "PersistedLoadPre",
-				callback = function()
-					for _, win in pairs(vim.api.nvim_tabpage_list_wins(0)) do
-						if vim.api.nvim_win_get_config(win).zindex then
-							vim.api.nvim_win_close(win, false)
-						end
-					end
-				end,
-			})
-			-- Close all plugin owned buffers before saving a session.
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "PersistedSavePre",
-				group = "rafi_persisted",
-				callback = function()
-					-- Detect if window is owned by plugin by checking buftype.
-					local current_buffer = vim.api.nvim_get_current_buf()
-					for _, win in ipairs(vim.fn.getwininfo()) do
-						local buftype = vim.bo[win.bufnr].buftype
-						if buftype ~= "" and buftype ~= "help" then
-							-- Delete plugin owned window buffers.
-							if win.bufnr == current_buffer then
-								-- Jump to previous window if current window is not a real file
-								vim.cmd.wincmd("p")
-							end
-							vim.api.nvim_buf_delete(win.bufnr, {})
-						end
-					end
-				end,
-			})
-			-- Before switching to a different session using Telescope, save and stop
-			-- current session to avoid previous session to be overwritten.
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "PersistedTelescopeLoadPre",
-				group = "rafi_persisted",
-				callback = function()
-					require("persisted").save()
-					require("persisted").stop()
-				end,
-			})
-			-- After switching to a different session using Telescope, start it so it
-			-- will be auto-saved.
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "PersistedTelescopeLoadPost",
-				group = "rafi_persisted",
-				callback = function(session)
-					require("persisted").start()
-					print("Started session " .. session.data.name)
-				end,
-			})
 		end,
 	},
 
@@ -412,17 +349,17 @@ return {
 	},
 
 	-----------------------------------------------------------------------------
-	{
-		"rest-nvim/rest.nvim",
-		ft = "http",
-		keys = {
-			{ "<Leader>mh", "<Plug>RestNvim", desc = "Execute HTTP request" },
-		},
-		opts = { skip_ssl_verification = true },
-		config = function(_, opts)
-			require("rest-nvim").setup(opts)
-		end,
-	},
+	-- {
+	-- 	"rest-nvim/rest.nvim",
+	-- 	ft = "http",
+	-- 	keys = {
+	-- 		{ "<Leader>mh", "<Plug>RestNvim", desc = "Execute HTTP request" },
+	-- 	},
+	-- 	opts = { skip_ssl_verification = true },
+	-- 	config = function(_, opts)
+	-- 		require("rest-nvim").setup(opts)
+	-- 	end,
+	-- },
 
 	-----------------------------------------------------------------------------
 	{
