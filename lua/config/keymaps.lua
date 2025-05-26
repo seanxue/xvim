927f1846683dbd48ebe336706314fe65290e2f0a
Sean Xue <lianggui.xue@gmail.com>
2025-05-26 15:41:14 +0800

    remove elite mode and formatting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -46,24 +46,11 @@ map({ 'n', 'x' }, '<leader>gG', '<leader>sW', { remap = true, desc = 'Visual sel
 -- }}}
 -- Navigation {{{
 
-if vim.F.if_nil(vim.g.elite_mode, false) then
-	-- Elite-mode: Arrow-keys resize window
-	map('n', '<Up>', '<cmd>resize +1<cr>', { desc = 'Increase Window Height' })
-	map('n', '<Down>', '<cmd>resize -1<cr>', { desc = 'Decrease Window Height' })
-	map('n', '<Left>', '<cmd>vertical resize +1<cr>', { desc = 'Increase Window Width' })
-	map('n', '<Right>', '<cmd>vertical resize -1<cr>', { desc = 'Decrease Window Width' })
-	unmap('n', { '<C-Up>', '<C-Down>', '<C-Left>', '<C-Right>' })
-else
-	-- Moves through display-lines, unless count is provided
-	map({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true, desc = 'Down' })
-	map({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = 'Up' })
-
-	-- Resize window using <ctrl> arrow keys
-	map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
-	map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
-	map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
-	map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
-end
+map('n', '<Up>', '<cmd>resize +1<cr>', { desc = 'Increase Window Height' })
+map('n', '<Down>', '<cmd>resize -1<cr>', { desc = 'Decrease Window Height' })
+map('n', '<Left>', '<cmd>vertical resize +1<cr>', { desc = 'Increase Window Width' })
+map('n', '<Right>', '<cmd>vertical resize -1<cr>', { desc = 'Decrease Window Width' })
+unmap('n', { '<C-Up>', '<C-Down>', '<C-Left>', '<C-Right>' })
 
 unmap('n', { '<S-h>', '<S-l>' })
 
@@ -95,9 +82,6 @@ map('n', '<A-}>', '<cmd>+tabmove<CR>', { desc = 'Tab Move Forwards' })
 
 -- }}}
 -- Selection {{{
-
-map('n', '<Leader><Leader>', 'V', { desc = 'Visual Mode' })
-map('x', '<Leader><Leader>', '<Esc>', { desc = 'Exit Visual Mode' })
 
 -- Select last paste
 map('n', 'vsp', "'`['.strpart(getregtype(), 0, 1).'`]'", { expr = true, desc = 'Select Paste' })
